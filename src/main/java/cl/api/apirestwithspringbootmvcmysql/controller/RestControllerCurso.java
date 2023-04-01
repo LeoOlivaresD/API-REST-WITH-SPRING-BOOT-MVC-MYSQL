b41e57579c44d087b6f5ae407515cc811fe6f2a2
LeoOlivaresD <cspraword@gmail.com>
2023-04-01 01:27:08 -0300

    Creación el método Post para crear cursos.

--- src/main/java/cl/api/apirestwithspringbootmvcmysql/controller/RestControllerCurso.java
+++ src/main/java/cl/api/apirestwithspringbootmvcmysql/controller/RestControllerCurso.java
@@ -3,6 +3,8 @@ import cl.api.apirestwithspringbootmvcmysql.model.Curso;
 import cl.api.apirestwithspringbootmvcmysql.service.CursoImplService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,6 +14,10 @@ public class RestControllerCurso {
     @Autowired
     CursoImplService cursoService;
 
+    @PostMapping(value = "/api/crear", headers = "Accept=application/json")
+    public void crearCurso(@RequestBody Curso curso){
+        cursoService.crearCurso(curso);
+    }
     @GetMapping(value ="/api/listar", headers = "Accept=application/json")
     public List<Curso> listaCursos(){
         return cursoService.listarCursos();
