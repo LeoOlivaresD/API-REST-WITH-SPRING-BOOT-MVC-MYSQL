d381dcc03dee874873c091fe916fc2479d0275f5
LeoOlivaresD <cspraword@gmail.com>
2023-04-01 00:44:11 -0300

    Agregamos notación @Repository

--- src/main/java/cl/api/apirestwithspringbootmvcmysql/repositoy/IRepositoryCurso.java
+++ src/main/java/cl/api/apirestwithspringbootmvcmysql/repositoy/IRepositoryCurso.java
@@ -1,6 +1,8 @@
 package cl.api.apirestwithspringbootmvcmysql.repositoy;
 import cl.api.apirestwithspringbootmvcmysql.model.Curso;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IRepositoryCurso extends JpaRepository<Curso, Integer> {
 }
