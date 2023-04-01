dc401140a5d7f67e479182306c81b74be0c45321
LeoOlivaresD <cspraword@gmail.com>
2023-04-01 00:06:06 -0300

    Clase test

--- src/test/java/cl/api/apirestwithspringbootmvcmysql/ApiRestWithSpringBootMvcMysqlApplicationTests.java
+++ src/test/java/cl/api/apirestwithspringbootmvcmysql/ApiRestWithSpringBootMvcMysqlApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApiRestWithSpringBootMvcMysqlApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
