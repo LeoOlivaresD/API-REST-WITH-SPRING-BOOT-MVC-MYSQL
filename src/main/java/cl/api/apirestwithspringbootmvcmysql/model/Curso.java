08b222efafbb10122c8dc840f2e882373b08c35a
LeoOlivaresD <cspraword@gmail.com>
2023-04-01 00:28:37 -0300

    Creacion de entidad Curso

---
+++ src/main/java/cl/api/apirestwithspringbootmvcmysql/model/Curso.java
@@ -0,0 +1,43 @@
+package cl.api.apirestwithspringbootmvcmysql.model;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+@Entity
+public class Curso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_curso")
+    private Integer idCurso;
+    @Column(name = "nivel_curso", length = 30, nullable = false)
+    private String nivelCurso;
+    @OneToMany
+    private List<Estudiantes> estudiantes = new ArrayList<>();
+
+    public Curso(){
+    }
+
+    public Integer getIdCurso() {
+        return idCurso;
+    }
+
+    public void setIdCurso(Integer idCurso) {
+        this.idCurso = idCurso;
+    }
+
+    public String getNivelCurso() {
+        return nivelCurso;
+    }
+
+    public void setNivelCurso(String nivelCurso) {
+        this.nivelCurso = nivelCurso;
+    }
+
+    public List<Estudiantes> getEstudiantes() {
+        return estudiantes;
+    }
+
+    public void setEstudiantes(List<Estudiantes> estudiantes) {
+        this.estudiantes = estudiantes;
+    }
+}
