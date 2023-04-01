29fd91edcecfa7184bdaa946163ead8ee24f2772
LeoOlivaresD <cspraword@gmail.com>
2023-04-01 00:05:01 -0300

    Start project

---
+++ src/main/java/cl/api/apirestwithspringbootmvcmysql/ApiRestWithSpringBootMvcMysqlApplication.java
@@ -0,0 +1,13 @@
+package cl.api.apirestwithspringbootmvcmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestWithSpringBootMvcMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestWithSpringBootMvcMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/api/apirestwithspringbootmvcmysql/ApiRestWithSpringBootMvcMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.api.apirestwithspringbootmvcmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestWithSpringBootMvcMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
